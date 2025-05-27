3b00bc12e623dba8bee1d73ad7d4fd65b26638ce
JP Juliao <jpjuliao@gmail.com>
2025-05-27 00:03:33 -0500

    Remoce unused act.

--- src/services/movieApi.test.tsx
+++ src/services/movieApi.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act, waitFor } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import { useMovies } from '../hooks/useMovies';
 import movieApi from './movieApi';
 
